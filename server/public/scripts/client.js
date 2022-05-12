d1855a19e2fcc04533500e8d9da76000cbaae3f8
Liz Horn <liz.mpls@gmail.com>
2022-05-12 14:23:33 -0500

    client-server-router handshake for GET

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -30,7 +30,17 @@ function setupClickListeners() {
 function getKoalas(){
   console.log( 'in getKoalas' );
   // ajax call to server to get koalas
-  
+  $.ajax({
+    method: 'GET',
+    url: '/koalas'
+  }).then(function(response){
+    console.log('back from /koalas GET');
+    //call function to display koalas
+
+  }).catch(function(err){
+    console.log('error in GET /koalas', err);
+    alert('Error getting koalas');
+  });
 } // end getKoalas
 
 function saveKoala( newKoala ){
--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -2,10 +2,14 @@ const express = require('express');
 const koalaRouter = express.Router();
 
 // DB CONNECTION
-
+/// array for now to test. replace w/ db connection
+let koalas = [];
 
 // GET
-
+koalaRouter.get('/', (req, res)=>{
+    console.log('/koalas GET');
+    res.send(koalas);
+});
 
 // POST
 
