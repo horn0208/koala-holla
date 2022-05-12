74e3bc3d081e3d805f00d59d805391bff015f0b2
Liz Horn <liz.mpls@gmail.com>
2022-05-12 14:29:30 -0500

    created pool.js

---
+++ server/routes/pool.js
@@ -0,0 +1,10 @@
+const pg = require('pg');
+const pool = new pg.Pool({
+    database: 'koala_holla_inventory',
+    host: 'Local',
+    port: 5432,
+    max: 12,
+    idleTimeoutMillis: 30000
+});
+
+module.exports = pool;
